1f275c71646098838d3d29446e9b5033bee28638
dungpd <pducdung.it@gmail.com>
2026-04-10 23:46:53 +0700

    chore: remove unused imports

--- xxxx-infrastructure/src/test/java/com/xxxx/ddd/infrastructure/persistence/repository/OrderInfrasRepositoryImplTest.java
+++ xxxx-infrastructure/src/test/java/com/xxxx/ddd/infrastructure/persistence/repository/OrderInfrasRepositoryImplTest.java
@@ -1,4 +1,4 @@
-package com.xxxx.ddd.infrastructue.persistence.repository;
+package com.xxxx.ddd.infrastructure.persistence.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
@@ -7,16 +7,15 @@ import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import com.xxxx.ddd.domain.model.entity.Order;
+import com.xxxx.ddd.infrastructure.persistence.mapper.OrderJPAMapper;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.xxxx.ddd.domain.model.entity.Order;
-import com.xxxx.ddd.infrastructure.persistence.mapper.OrderJPAMapper;
-import com.xxxx.ddd.infrastructure.persistence.repository.OrderInfrasRepositoryImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderInfrasRepositoryImplTest {
